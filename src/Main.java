3e93583b37c6fd8e507573cb68b3b66b724c4722
geugenio <geugnio12@gmail.com>
2025-09-30 18:59:19 -0300

    classe main para testes finalizada.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        ServicoDeEntrega entrega10km = new ServicoDeEntrega();
+        Funcionario f1 = new Funcionario("Gabriel", 22);
+        Carro c1 = new Carro("ABC1234", 2025);
+        entrega10km.addFretavel(f1);
+        entrega10km.addFretavel(c1);
+        System.out.println("Entrega de 10km feita por " + f1.getNome() +
+                " em carro de placa " + c1.getPlaca() + ". Total: " + entrega10km.calcularCustoTotal(10)  + " R$.");
+
+       ServicoDeEntrega entrega25km = new ServicoDeEntrega();
+       Autonomo a1 = new Autonomo("Jose", 25);
+       Motocicleta m1 = new Motocicleta("AAA1111", 2025);
+       entrega25km.addFretavel(a1);
+       entrega25km.addFretavel(m1);
+
+       System.out.println("Entrega de 25km feita por " + a1.getNome() +
+                " em moto de placa " + m1.getPlaca() + ". Total: " + entrega25km.calcularCustoTotal(25) + " R$.");
     }
 }
