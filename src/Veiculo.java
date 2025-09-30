99218df57649f3aa71297e8002b46c4a7796661a
geugenio <geugnio12@gmail.com>
2025-09-30 18:59:01 -0300

    Classe de veiculo tem getters e setters

--- src/Veiculo.java
+++ src/Veiculo.java
@@ -1,9 +1,25 @@
 public abstract class Veiculo {
-    public String placa;
-    public int ano;
+    private String placa;
+    private int ano;
 
     public Veiculo(String placa, int ano) {
         this.placa = placa;
         this.ano = ano;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
     }
 }
