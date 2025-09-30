5746fdc5a3df247dc79cc3d354f4ccc96c024610
geugenio <geugnio12@gmail.com>
2025-09-30 18:58:40 -0300

    finalização da classe para serviço de entrega

--- src/ServicoDeEntrega.java
+++ src/ServicoDeEntrega.java
@@ -1,2 +1,29 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServicoDeEntrega {
+    private List<Fretavel> fretaveis;
+
+    public ServicoDeEntrega(){
+        this.fretaveis = new ArrayList<>();
+    }
+
+    public void addFretavel(Fretavel fretavel){
+        fretaveis.add(fretavel);
+    }
+    public double calcularCustoTotal(double distanciaEmKm){
+        double total = 0;
+        for(Fretavel f : fretaveis){
+            total += f.calcularCustoFrete(distanciaEmKm); // Exemplo com 10 km
+        }
+        return total;
+    }
+
+    public List<Fretavel> getFretaveis() {
+        return fretaveis;
+    }
+
+    public void setFretaveis(List<Fretavel> fretaveis) {
+        this.fretaveis = fretaveis;
+    }
 }
